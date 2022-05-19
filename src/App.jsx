b5049a5876c53d94081958691ef5346b8216ad17
Yantimir <mingazitdinov@mail.ru>
2022-05-19 23:56:55 +0400

    mini app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import React from "react";
-import { Layout } from 'antd';
+import React, { useState } from "react";
+import { Layout, Row, Col, Slider } from 'antd';
+import { MenuOutlined } from "@ant-design/icons";
+
+import Table from "./components/Table/Table";
+
+
 const { Header, Footer, Content } = Layout;
 
 export const App = () => {
 
+    const [rows, setRows] = useState(20);
+
     return (
-        <>
+        <div className="container">
             <Layout>
-                <Header>Header</Header>
-                <Content>Content</Content>
-                <Footer>Footer</Footer>
+                <Header><MenuOutlined style={{ marginTop:"15px", fontSize: '32px', color: '#08c' }}/></Header>
+                <Content>
+                    <Row>
+                        <Col span={16} offset={4}>
+                            <Slider min={10} max={100} onChange={setRows} defaultValue={rows}/>
+                            <Table rows={rows}/>
+                        </Col>
+                    </Row>
+                </Content>
+                <Footer style={{margin: "0 auto"}}>made with by Ant Design © 2022</Footer>
             </Layout>
-        </>
+        </div>
     );
 };
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,29 +1,115 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Typography, Image } from 'antd';
+import { pokemonData } from "../../pokemon.js";
 
 const columns = [
     {
         title: 'Name',
         dataIndex: 'name',
         key: 'name',
+        render: (text) => <Typography.Text copyable>{text}</Typography.Text>
     },
     {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
+        title: 'Number',
+        dataIndex: 'number',
+        key: 'number',
     },
     {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
+        title: 'Class',
+        dataIndex: 'classification',
+        key: 'classification',
+        filters: [
+            {
+                text: "Seed Pokémon",
+                value: "Seed Pokémon"
+            },
+            {
+                text: "Lizard Pokémon",
+                value: "Lizard Pokémon"
+            },
+            {
+                text: "Flame Pokémon",
+                value: "Flame Pokémon"
+            },
+            {
+                text: "Tiny Turtle Pokémon",
+                value: "Tiny Turtle Pokémon"
+            },
+            {
+                text: "Turtle Pokémon",
+                value: "Turtle Pokémon"
+            },
+            {
+                text: "Worm Pokémon",
+                value: "Worm Pokémon"
+            },
+            {
+                text: "Bird Pokémon",
+                value: "Bird Pokémon"
+            },
+            {
+                text: "Tiny Bird Pokémon",
+                value: "Tiny Bird Pokémon"
+            },
+            {
+                text: "Poison Bee Pokémon",
+                value: "Poison Bee Pokémon"
+            },
+            {
+                text: "Mouse Pokémon",
+                value: "Mouse Pokémon"
+            },
+            {
+                text: "Cocoon Pokémon",
+                value: "Cocoon Pokémon"
+            },
+            {
+                text: "Butterfly Pokémon",
+                value: "Butterfly Pokémon"
+            },
+            {
+                text: "Hairy Pokémon",
+                value: "Hairy Pokémon"
+            },
+        ],
+        onFilter: (value, record) => record.classification.includes(value)
+    },
+    {
+        title: 'Maximum HP',
+        dataIndex: 'maxHP',
+        key: 'maxHP',
+        sorter: (a, b) => a.maxHP - b.maxHP,
+    },
+    {
+        title: 'Maximum CP',
+        dataIndex: 'maxCP',
+        key: 'maxCP',
+    },
+    {
+        title: 'Image',
+        dataIndex: 'image',
+        key: 'image',
+        render: (link) => <Image src={link} width={150} />
     },
 ];
 
-export const _Table = () => {
+const dataPokemons = pokemonData.map((item) => ({ ...item, key: item.id }));
+
+const _Table = ({ rows=20 }) => {
 
     return (
         <>
-            <Table />
+            <Table
+                dataSource={dataPokemons}
+                columns={columns}
+                pagination={{
+                    pageSize: rows,
+                    // defaultPageSize: "20", // количество по умоллчанию
+                    showSizeChanger: false, // включить выбор количества
+                    // pageSizeOptions: [20, 50, 100] // варианты выбора
+                }}
+            />
         </>
     );
 }
+export default _Table;
